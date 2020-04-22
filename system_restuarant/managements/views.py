b1b318bd430578b3af59c532130a3cb4754942f6
veerapong <poom.vrp@gmail.com>
2020-04-22 20:13:43 +0700

    view&templete
    update

--- system_restuarant/managements/views.py
+++ system_restuarant/managements/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 # Create your views here.
+def home(request):
+    
+    return HttpResponse('aaa')
+    
