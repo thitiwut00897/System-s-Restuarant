44e8f2265955154710197d5ce31fae9cca3fc211
Rungwarapon <42974200+Rungwarapon@users.noreply.github.com>
2020-04-26 22:43:50 +0700

    -แก้ฟอร์ม -เพิ่มรูป

--- system_restuarant/managements/urls.py
+++ system_restuarant/managements/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('login',views.my_login,name='login'),
 
     # menagement Restuarant&Menu
-    path('restaurant', views.management, name='management'),
+    path('restaurant/', views.management, name='management'),
     path('restaurant/add/', views.addRestaurant, name='addRestaurant'),
     path('restaurant/food/add/<int:id>/', views.addFood, name='addFood'),
     path('restautant/delete/<int:id>/', views.deleteRestaurant, name='deleteRestaurant'),
-    path('restaurant/food/delete/<int:id>/', views.deleteFood, name='deleteFood')
-    # path('restaurant/edit/<int:id>/', views.editRestaurant, name='editRestaurant')
+    path('restaurant/food/<int:res_id>/delete/<int:food_id>/', views.deleteFood, name='deleteFood')
+    # path('restaurant/edit/<int:id>/', views.editRestaurant, name='editRestaurant'),
+    # path('restaurant/food/edit/<int:id>/', views.editFood, name='editFood')
 ]
