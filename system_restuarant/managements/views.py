dc011a6957ceb70f9f0aee973a4d8ecbfdd3059f
veerapong <poom.vrp@gmail.com>
2020-04-27 17:46:19 +0700

    Revert "-"
    This reverts commit 807f5e8b28988a81c1145a3e4b25a4f01c9d45bb.

--- system_restuarant/managements/views.py
+++ system_restuarant/managements/views.py
@@ -76,9 +76,7 @@ def addRestaurant(request):
         'restaurant': restaurant
     })
 
-
 def editRestaurant(request, id):
-    restaurant = Restaurant.objects.all()
     restaurant = Restaurant.objects.get(restaurant_id=id)
     # success = ''
     if request.method == 'POST':
@@ -94,6 +92,7 @@ def editRestaurant(request, id):
         # 'success': success
     })
 
+
 def deleteRestaurant(request, id):
     restaurant = Restaurant.objects.get(restaurant_id=id)
     restaurant.delete()
@@ -102,12 +101,15 @@ def deleteRestaurant(request, id):
 
 def addFood(request, id):
     fd = Food.objects.filter(restaurant_id=id)
+    # print(fd)
     if request.method == 'POST':
         form = AddFoodForm(request.POST, request.FILES)
         if form.is_valid():
+            # Food.objects.create(food_name = request.POST.get('food_name'), picture = request.POST.get('picture'), price = request.POST.get('price'), restaurant_restaurant_id_id = id )
             food = form.save(commit=False)
             food.restaurant_id = id
             food.save()
+            # print(food.picture)
             return redirect('addFood', id=id)
     else:
         form = AddFoodForm()
