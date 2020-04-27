682a12d44b8a276fe4319406b0fda23eb6ee3c4a
Naphat Pornbunruang <Naphat4566@gmail.com>
2020-04-27 23:01:08 +0700

    .

--- system_restuarant/managements/forms.py
+++ system_restuarant/managements/forms.py
@@ -25,7 +25,8 @@ class AddRestaurantForm(ModelForm):
             cleaned_data = super().clean()
             if( price.isdigit() == false):
                 msg = "กรุณากรอกตัวเลข"
-                self.add_error('price', msg)                
+                self.add_error('price', msg) 
+
 class EditRestaurantForm(ModelForm):
     class Meta:
         model = Restaurant
@@ -38,7 +39,7 @@ class EditRestaurantForm(ModelForm):
         }
         widgets = {
             'restaurant_name': forms.TextInput(attrs={'class':'form-control'}),
-            'working_hours': forms.TextInput(attrs={'class':'form-control'}),
+            'open_time': forms.TextInput(attrs={'class':'form-control'}),
             'close_time': forms.TextInput(attrs={'class':'form-control'})
         }
 
@@ -62,7 +63,7 @@ class AddFoodForm(ModelForm):
         #         msg = "กรุณากรอกตัวเลข"
         #         self.add_error('price', msg)   
         # 
-        
+
 class EditFoodForm(ModelForm):
     class Meta:
         model = Food
--- system_restuarant/managements/urls.py
+++ system_restuarant/managements/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     # menagement Restuarant&Menu
     path('restaurant/', views.management, name='management'),
     path('restaurant/add/', views.addRestaurant, name='addRestaurant'),
-    # path('restaurant/homepage/', views.homepage, name='homepage'),
+    # path('restaurant/detailRestaurant/',
+    #      views.detailRestaurant, name='detailRestaurant'),
     path('restaurant/food/add/<int:id>/', views.addFood, name='addFood'),
     path('restautant/delete/<int:id>/',
          views.deleteRestaurant, name='deleteRestaurant'),
