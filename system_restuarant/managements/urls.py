348528c787318c55cdb0e8bfe9816e0e2ef9e8c5
Naphat Pornbunruang <Naphat4566@gmail.com>
2020-04-27 18:09:59 +0700

    homepage

--- system_restuarant/managements/urls.py
+++ system_restuarant/managements/urls.py
@@ -4,19 +4,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login',views.my_login,name='login'),
-    path('logout',views.my_logout,name='logout'),
+    path('', views.homepage, name='homepage'),
+    path('login', views.my_login, name='login'),
+    path('logout', views.my_logout, name='logout'),
 
     # menagement Restuarant&Menu
     path('restaurant/', views.management, name='management'),
     path('restaurant/add/', views.addRestaurant, name='addRestaurant'),
     path('restaurant/homepage/', views.homepage, name='homepage'),
     path('restaurant/food/add/<int:id>/', views.addFood, name='addFood'),
-    path('restautant/delete/<int:id>/', views.deleteRestaurant, name='deleteRestaurant'),
-    path('restaurant/food/<int:res_id>/delete/<int:food_id>/', views.deleteFood, name='deleteFood'),
+    path('restautant/delete/<int:id>/',
+         views.deleteRestaurant, name='deleteRestaurant'),
+    path('restaurant/food/<int:res_id>/delete/<int:food_id>/',
+         views.deleteFood, name='deleteFood'),
     path('restaurant/edit/<int:id>/', views.editRestaurant, name='editRestaurant'),
-    path('manageOrder',views.manageOrder,name='manageOrder')
+    path('manageOrder', views.manageOrder, name='manageOrder')
     # path('restaurant/edit/<int:id>/', views.editRestaurant, name='editRestaurant'),
     # path('restaurant/food/edit/<int:id>/', views.editFood, name='editFood')
 ]
