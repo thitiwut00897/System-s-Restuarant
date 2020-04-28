277b912a6cffbded9bb15fb2f1889599cdfc5366
Naphat Pornbunruang <Naphat4566@gmail.com>
2020-04-29 00:32:38 +0700

    เพิ่ม food_id

--- system_restuarant/managements/views.py
+++ system_restuarant/managements/views.py
@@ -36,6 +36,7 @@ def homepage(request):
 
 def detailRestaurant(request, id):
     res_id = Restaurant.objects.get(restaurant_id=id)
+    food_id = Food.objects.get(food_id=res_id.restaurant_id)
     list = []
     dict = {
         'restaurant_id': res_id.restaurant_id,
@@ -45,10 +46,16 @@ def detailRestaurant(request, id):
         'types': res_id.types,
         'owner': res_id.owner,
         'picture_restaurant': res_id.picture_restaurant,
+        'food_id': food_id,
+        'food_name': food_name,
+        'picture_food': picture,
+        'price': price,
+        'restaurant': restaurant
     }
     list.append(dict)
     return render(request, 'detailRestaurant.html', context={
         'id': id,
+        'food_id': food_id,
         'res_id': list
     })
 
@@ -81,12 +88,12 @@ def my_logout(request):
 def addRestaurant(request):
     if request.method == 'POST':
         form = AddRestaurantForm(request.POST, request.FILES)
-        
         if form.is_valid():
             typeRestaurant = Type.objects.create(
                 type_name=request.POST.get('type_name'))
             restaurant = form.save(commit=False)
             restaurant.type_type_id = typeRestaurant
+            # print(restaurant.picture_restaurant)
             restaurant.save()
             return redirect('management')
     else:
@@ -99,12 +106,12 @@ def addRestaurant(request):
 def editRestaurant(request, id):
     restaurant = Restaurant.objects.get(restaurant_id=id)
     if request.method == 'POST':
-        form = AddtRestaurantForm(request.POST, instance=restaurant)
+        form = EditRestaurantForm(request.POST, instance=restaurant)
         if form.is_valid():
             form.save()
             return redirect(to='management')
     else:
-        form = AddRestaurantForm(instance=restaurant)
+        form = EditRestaurantForm(instance=restaurant)
 
     return render(request, 'editRestaurant.html', context={
         'form': form,
@@ -130,10 +137,9 @@ def managementFood(request, id):
 def addFood(request, res_id):
     if request.method == 'POST':
         form = AddFoodForm(request.POST, request.FILES)
-        msg = 'เพิ่มรายการอาหาร'
         if form.is_valid():
             food = form.save(commit=False)
-            food.restaurant_id = res_id
+            food.restaurant_id = id
             food.save()
             return redirect('managementFood', id=res_id)
     else:
@@ -148,12 +154,12 @@ def editFood(request, res_id, food_id):
     food = Food.objects.get(food_id=food_id)
     if request.method == 'POST':
         if food.restaurant_id == res_id:
-            form = AddFoodForm(request.POST, instance=food)
+            form = EditFoodForm(request.POST, instance=food)
             if form.is_valid():
                 form.save()
                 return redirect('managementFood', id=res_id)
     else:
-        form = AddFoodForm(instance=food)
+        form = EditFoodForm(instance=food)
     return render(request, 'editFood.html', context={
         'form': form,
         'id': food_id,
