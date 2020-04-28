c7e487d752f4c44ef3df0cb9f186b6f3538345c5
Naphat Pornbunruang <Naphat4566@gmail.com>
2020-04-28 23:52:48 +0700

    แก้ homepage

--- system_restuarant/managements/urls.py
+++ system_restuarant/managements/urls.py
@@ -9,19 +9,25 @@ urlpatterns = [
     path('logout', views.my_logout, name='logout'),
 
     # menagement Restuarant&Menu
-    path('restaurant/detailRestaurant/<int:id>/', views.detailRestaurant, name='detailRestaurant'),
+    path('restaurant/detailRestaurant/<int:id>/',
+         views.detailRestaurant, name='detailRestaurant'),
 
     path('restaurant/', views.management, name='management'),
     path('restaurant/add/', views.addRestaurant, name='addRestaurant'),
     path('restaurant/edit/<int:id>/', views.editRestaurant, name='editRestaurant'),
-    path('restautant/delete/<int:id>/',views.deleteRestaurant, name='deleteRestaurant'),
+    path('restautant/delete/<int:id>/',
+         views.deleteRestaurant, name='deleteRestaurant'),
 
     path('restaurant/food/<int:id>/', views.managementFood, name='managementFood'),
     path('restaurant/food/<int:res_id>/add/', views.addFood, name='addFood'),
-    path('restaurant/food/<int:res_id>/delete/<int:food_id>/', views.deleteFood, name='deleteFood'),
-    path('restaurant/food/<int:res_id>/edit/<int:food_id>/', views.editFood, name='editFood'),
+    path('restaurant/food/<int:res_id>/delete/<int:food_id>/',
+         views.deleteFood, name='deleteFood'),
+    path('restaurant/food/<int:res_id>/edit/<int:food_id>/',
+         views.editFood, name='editFood'),
 
     path('manageOrder', views.manageOrder, name='manageOrder'),
-    path('Order/confirmOrder/<int:order_id>/',views.confirmOrder, name='confirmOrder'),
-    path('Order/cancelOrder/<int:order_id>/',views.cancelOrder, name='cancelOrder')
+    path('Order/confirmOrder/<int:order_id>/',
+         views.confirmOrder, name='confirmOrder'),
+    path('Order/cancelOrder/<int:order_id>/',
+         views.cancelOrder, name='cancelOrder')
 ]
