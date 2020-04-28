e2647ce34b21e73f8deaade4f51319c1675fc196
Rungwarapon <42974200+Rungwarapon@users.noreply.github.com>
2020-04-28 23:52:12 +0700

    แก้form.py

--- system_restuarant/managements/views.py
+++ system_restuarant/managements/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from classes.models import Food, Owner, Restaurant, Type, Order, Order_List
-from managements.forms import AddFoodForm, AddRestaurantForm, EditRestaurantForm, EditFoodForm
+from managements.forms import AddFoodForm, AddRestaurantForm
 
 
 def home(request):
@@ -81,6 +81,7 @@ def my_logout(request):
 def addRestaurant(request):
     if request.method == 'POST':
         form = AddRestaurantForm(request.POST, request.FILES)
+        
         if form.is_valid():
             typeRestaurant = Type.objects.create(
                 type_name=request.POST.get('type_name'))
@@ -98,12 +99,12 @@ def addRestaurant(request):
 def editRestaurant(request, id):
     restaurant = Restaurant.objects.get(restaurant_id=id)
     if request.method == 'POST':
-        form = EditRestaurantForm(request.POST, instance=restaurant)
+        form = AddtRestaurantForm(request.POST, instance=restaurant)
         if form.is_valid():
             form.save()
             return redirect(to='management')
     else:
-        form = EditRestaurantForm(instance=restaurant)
+        form = AddRestaurantForm(instance=restaurant)
 
     return render(request, 'editRestaurant.html', context={
         'form': form,
@@ -129,6 +130,7 @@ def managementFood(request, id):
 def addFood(request, res_id):
     if request.method == 'POST':
         form = AddFoodForm(request.POST, request.FILES)
+        msg = 'เพิ่มรายการอาหาร'
         if form.is_valid():
             food = form.save(commit=False)
             food.restaurant_id = res_id
@@ -146,12 +148,12 @@ def editFood(request, res_id, food_id):
     food = Food.objects.get(food_id=food_id)
     if request.method == 'POST':
         if food.restaurant_id == res_id:
-            form = EditFoodForm(request.POST, instance=food)
+            form = AddFoodForm(request.POST, instance=food)
             if form.is_valid():
                 form.save()
                 return redirect('managementFood', id=res_id)
     else:
-        form = EditFoodForm(instance=food)
+        form = AddFoodForm(instance=food)
     return render(request, 'editFood.html', context={
         'form': form,
         'id': food_id,
